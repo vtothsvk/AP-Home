165c0b717dd71efbafe513fb4a30ce6f47336882
vtothsvk <v.toth.svk@gmail.com>
2020-12-01 19:22:53 +0100

    so far so good

---
+++ AP_Nurse_config.h
@@ -0,0 +1,15 @@
+#ifndef AP_CONFIG_H
+#define AP_CONFIG_H
+
+#include "AP_Nurse_types.h"
+
+#define BED
+#define DOOR
+#define HALLWAY
+#define KITCHEN
+
+#define STUCK_TIMER_DELAY   5//s
+
+
+
+#endif
