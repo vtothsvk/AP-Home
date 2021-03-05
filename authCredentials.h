17d42041b53179c47ad6ea10547bdc730fc55ce5
viktor.toth <viktor.toth@r-das.sk>
2021-03-05 12:12:06 +0100

    modified wifi manager and added configurable data advertisement

---
+++ authCredentials.h
@@ -0,0 +1,17 @@
+#ifndef authCredentials_h
+#define authCredentials_h
+
+const char SN[] = "ceho202110000002";
+
+const char PRIVATE_KEY[] = 
+"-----BEGIN EC PRIVATE KEY-----\n" 
+"MHcCAQEEIDEEpXbC4C/yzZiL66RDcOUN/mRxJMuOiiwTWanS9uVjoAoGCCqGSM49\n" 
+"AwEHoUQDQgAEqtc8CnEjJdUNJOUJclZup0A+VMxR/hQ6tWZBl08DNR470HMOZsxT\n" 
+"CUZ2LMdTX1uBsrkASxQ9j58fzCqvGs1biw==\n" 
+"-----END EC PRIVATE KEY-----\n" 
+;
+const char kid[] = "c36c8cd355bdddeecc4117933e74d9e684328c6cc23628dca341c0e206a3b822";
+
+const char myId[] = "08d8df1b-a44c-4ac7-81bc-3cb660b107f7";
+
+#endif
