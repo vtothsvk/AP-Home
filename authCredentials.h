1d0f07a11228a58713b51518ee5deff3ad150061
vtothsvk <52499237+vtothsvk@users.noreply.github.com>
2021-03-05 12:37:44 +0100

    Update authCredentials.h

--- authCredentials.h
+++ authCredentials.h
@@ -1,17 +1,13 @@
 #ifndef authCredentials_h
 #define authCredentials_h
 
-const char SN[] = "ceho202110000002";
+const char SN[] = "DEV_SN";
 
 const char PRIVATE_KEY[] = 
-"-----BEGIN EC PRIVATE KEY-----\n" 
-"MHcCAQEEIDEEpXbC4C/yzZiL66RDcOUN/mRxJMuOiiwTWanS9uVjoAoGCCqGSM49\n" 
-"AwEHoUQDQgAEqtc8CnEjJdUNJOUJclZup0A+VMxR/hQ6tWZBl08DNR470HMOZsxT\n" 
-"CUZ2LMdTX1uBsrkASxQ9j58fzCqvGs1biw==\n" 
-"-----END EC PRIVATE KEY-----\n" 
+"DEV_PK"
 ;
-const char kid[] = "c36c8cd355bdddeecc4117933e74d9e684328c6cc23628dca341c0e206a3b822";
+const char kid[] = "DEV_KID";
 
-const char myId[] = "08d8df1b-a44c-4ac7-81bc-3cb660b107f7";
+const char myId[] = "DEV_ID";
 
 #endif
